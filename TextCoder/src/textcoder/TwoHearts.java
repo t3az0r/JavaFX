20ef4c8eb1e433b3e7d9d0a9553661f17d655349
Christian Thies <thies@hogrefe.de>
2014-12-29 15:26:50 +0100

    font cover

---
+++ TextCoder/src/textcoder/TwoHearts.java
@@ -0,0 +1,7 @@
+public class TwoHearts {
+  public static final String TWO_HEARTS = "\uD83D\uDC95";
+  public static void main (String[] args) {
+    System.out.println("\uD83D\uDC95");
+    System.out.println("### "+TWO_HEARTS+" ###");
+  }
+}
