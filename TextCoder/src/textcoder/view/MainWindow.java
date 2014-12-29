60755703bf96e68e703a3d66e29af48ba20d995e
Christian Thies <thies@hogrefe.de>
2014-12-29 15:32:34 +0100

    modified layout

--- TextCoder/src/textcoder/view/MainWindow.java
+++ TextCoder/src/textcoder/view/MainWindow.java
@@ -36,26 +36,51 @@ public class MainWindow extends Application {
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
         
-        String decoration = TWO_HEARTS;
+        String decoration = HEAVY_BLACK_HEART;
+        String decoration2 = BLACK_HEART;
         
-        Text scenetitle = new Text(decoration+" 0x 111505050e "+decoration);
-        //scenetitle.setFont(Font.font("OCR A Extended", FontWeight.NORMAL, 48));
+        Text scenetitle = new Text(" "+decoration2+" 0x 111505050e "+decoration2);
+//        scenetitle.setFont(Font.font("OCR A Extended", FontWeight.NORMAL, 48));
+//        scenetitle.setFont(Font.font("DejaVu Sans", FontWeight.NORMAL, 48));
         grid.add(scenetitle, 0, 0);
+
+//        for(String fn: Font.getFontNames()) {
+//            System.out.println(fn);
+//        }
+//        
+//        Text scenetitle2 = new Text(decoration+" 0x 111505050e "+decoration);
+//        scenetitle2.setFont(Font.font("Arial Unicode MS", FontWeight.BOLD, 48));
+//        scenetitle2.setFont(Font.font("Quivira", FontWeight.NORMAL, 48));
+//        scenetitle2.setFont(Font.font("Apple Color Emoji", 48));
+//        grid.add(scenetitle2, 0, 1);
 
         Text scenesubtitle = new Text("140805 0c0f1605 130f0e0713");
         //scenesubtitle.setFont(Font.font("OCR A Extended", FontWeight.NORMAL, 36));
         grid.add(scenesubtitle, 0, 1);
 
+        Text spacer1 = new Text(" ");
+        spacer1.setFont(Font.font("OCR A Extended", FontWeight.NORMAL, 48));
+        grid.add(spacer1, 0, 2);
+
+        Text spacer2 = new Text("  ");
+        spacer2.setFont(Font.font("OCR A Extended", FontWeight.NORMAL, 48));
+        grid.add(spacer2, 0, 3);
+
         scenetitle.setId("welcome-text");
         scenesubtitle.setId("welcome-subtext");
         
-        Scene scene = new Scene(grid, 800, 250);
+        Scene scene = new Scene(grid, 1250, 1200);
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         scene.getStylesheets().add(MainWindow.class.getResource("MainWindow.css").toExternalForm());
         
         primaryStage.show();
+        System.out.println("###"+TWO_HEARTS);
     }
+    
+    public static final String HEAVY_BLACK_HEART = "\u2764";
+    public static final String WHITE_HEART = "\u2661";
+    public static final String BLACK_HEART = "\u2665";
     public static final String TWO_HEARTS = "\uD83D\uDC95";
     public static final String SIMPLE_DASH = "-";
 
@@ -63,9 +88,7 @@ public class MainWindow extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("###"+TWO_HEARTS);
         launch(args);
-        System.out.println("###"+TWO_HEARTS);
     }
     
 }
